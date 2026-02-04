fd2affafb2c8c0d1dffec00e321e4671a6a2c218
Vansh0827 <vanshpat208@gmail.com>
2026-02-03 20:11:15 -0500

    second commit

--- src/components/Valentine.jsx
+++ src/components/Valentine.jsx
@@ -5,7 +5,7 @@ const noMessages = [
     "Oh? 👀 You clicked No already?",
     "Wow. Bold choice. Very bold.",
     "Are you *sure* sure? Because my heart disagrees.",
-    "Khushboo… that button is starting to look suspicious 😏",
+    "Diyuu… that button is starting to look suspicious 😏",
     "Okay but imagine saying No to *this face* 🥺",
     "This is getting awkward. Just say Yes already.",
     "At this point the No button is just decoration 😂",
@@ -68,7 +68,7 @@ export default function Valentine() {
     const [yesScale, setYesScale] = useState(1);
     const [noStage, setNoStage] = useState(0);
 
-    const name = "Khushboo";
+    const name = "Diyu";
 
     useEffect(() => {
         const canvas = canvasRef.current;
